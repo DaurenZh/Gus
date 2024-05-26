54e0b8ace3e0c05f217f6fbb88236ada263132b7
Ablikon <ablik2005@gmail.com>
2024-05-26 20:07:17 +0600

    enter

--- baran/src/pages/Shop.jsx
+++ baran/src/pages/Shop.jsx
@@ -7,42 +7,42 @@ function Shop() {
   return (
     <>
       <section className="popular-sales"> 
-        <h2 className="heading">Popular Sales</h2> 
-            <div className="view-more-container"> 
-              <button className="view-more">View More</button> 
-              <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/365c896af0c331c3bac4d78633897473db8dc6de805a186777e0d9ce2e74017e?apiKey=145a614dc8264552af1ec45aa2fef7a9&" className="arrow-icon" alt="" /> 
+        <h2 className="headingbek">Popular Sales</h2> 
+            <div className="view-more-containerbek"> 
+              <button className="view-morebek">View More</button> 
+              <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/365c896af0c331c3bac4d78633897473db8dc6de805a186777e0d9ce2e74017e?apiKey=145a614dc8264552af1ec45aa2fef7a9&" className="arrow-iconbek" alt="" /> 
             </div> 
       </section> 
-      <div className="popular">
+      <div className="popularbek">
         <div className="cards">
         <img
             loading="lazy"
             src={card}
-            className="canopoly"
+            className="canopolyhan"
             alt="cat"
           />
           <img
             loading="lazy"
             src={card}
-            className="canopoly"
+            className="canopolyhan"
             alt="cat"
           />
           <img
             loading="lazy"
             src={card}
-            className="canopoly"
+            className="canopolyhan"
             alt="cat"
           />
           <img
             loading="lazy"
             src={card}
-            className="canopoly"
+            className="canopolyhan"
             alt="cat"
           />
         </div>
       </div>
-      <header className="header">
-          <h1 className="title">Categories</h1>
+      <header className="headerbek">
+          <h1 className="titlebek">Categories</h1>
       </header>
       <div className="popularbek">
         <div className="cardsbek">
